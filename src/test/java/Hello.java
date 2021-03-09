2141635b1b846eacb64c749079f126f6dfb59437
esrabuyuktas <72354664+esrabuyuktas@users.noreply.github.com>
2021-03-09 10:09:19 +0000

    Added changes to hello from Github

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("HELLO really AWESOME JAVA");
+        System.out.println("HELLO really FROM GITHUB MADE SOME CHANGES  AWESOME JAVA");
 
     }
 }
