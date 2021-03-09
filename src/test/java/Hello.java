32e1cf8ebca09c71908a67a6bcc718d7320a0cfc
esrabuyuktas <esrabuyuktas6@gmail.com>
2021-03-09 10:15:26 +0000

    added comments on same line

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,6 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("HELLO really AWESOME JAVA");
+        System.out.println("HELLO really AWESOME JAVA");//added comment
+
+
 
     }
 }
